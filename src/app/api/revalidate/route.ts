ac6739fc312f538b997e1c19478c057140813676
Alex Hramovich <alex.gramovich.1996@gmail.com>
2024-12-20 14:56:20 +0100

    update revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,16 +1,18 @@
 import type { NextRequest } from 'next/server'
 import { revalidatePath, revalidateTag } from 'next/cache'
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
     const tag = request.nextUrl.searchParams.get('tag')
-    const slug = request.nextUrl.searchParams.get('slug')
+    const data = await request.json();
 
-    if (!tag || !slug) {
+    if (!tag || !data.full_slug) {
         return Response.json({ error: 'query parameter is required' }, { status: 400 })
     }
 
+    const correctSlug = data.full_slug === 'home' ? '/' : data.full_slug
+
     revalidateTag(tag)
-    revalidatePath(slug || '/')
+    revalidatePath(correctSlug)
 
     return Response.json({ revalidated: true, now: Date.now() })
 }
