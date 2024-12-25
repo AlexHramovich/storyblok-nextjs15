54ee5d3ceb1a7a5266d0fb649bfb49cb2886a104
Alex Hramovich <alex.gramovich.1996@gmail.com>
2024-12-25 14:11:33 +0100

    update preview revalidation

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,5 +1,6 @@
 import { ISbStoriesParams, StoryblokClient, StoryblokStory } from "@storyblok/react/rsc";
 import { getStoryblokApi } from "@/lib/storyblok";
+// import { useEffect, useState } from "react";
 
 export async function generateStaticParams() {
   return [];
@@ -9,17 +10,43 @@ const fetchStory = async (slug?: string[]) => {
   const sbParams: ISbStoriesParams = { version: process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_VERSION };
   const storyblokApi: StoryblokClient = getStoryblokApi();
 
-  
-
-  return storyblokApi.get(`cdn/stories/${slug ? slug.join('/') : 'home'}`, sbParams, { next: {  revalidate: process.env.NODE_ENV === 'production' ? 3600 : 0 }, });
+  return storyblokApi.get(
+    `cdn/stories/${slug ? slug.join('/') : 'home'}`,
+    sbParams,
+    {
+      cache: process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_VERSION === 'draft' ? 'no-store' : 'force-cache',
+      next: {
+        tags: ['cms']
+      }
+    });
 }
 
 type Params = Promise<{ slug?: string[] }>;
 
 export default async function Home({ params }: { params: Params }) {
-  const page = await fetchStory((await params).slug);
+  const pageData = await fetchStory((await params).slug);
+  // const [previewPageData, setPreviewPageData] = useState<null | ISbResult>(null);
 
+  // const fetchPreviewData = async () => {
+  //   const previewData = await fetchStory((await params).slug);
+  //   setPreviewPageData(previewData);
+  // }
+
+  // useEffect(() => {
+  //   if (process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_VERSION === 'draft') {
+  //     fetchPreviewData();
+  //   }
+  // }, [])
+
+  // if (process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_VERSION === 'draft' && previewPageData) {
+  //   return (
+  //     <StoryblokStory story={previewPageData.data.story} />
+  //   );
+  // }
+
+  // if (process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_VERSION !== 'draft') {
   return (
-    <StoryblokStory story={page.data.story} />
+    <StoryblokStory story={pageData.data.story} />
   );
+  // }
 }
